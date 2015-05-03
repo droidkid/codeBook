340a65f884482f6c2051576a43dde50864746d1f
droidkid <karch08@gmail.com>
2015-05-04 00:16:24 +0530

    trying heroku

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DB_HOST = 'ec2-184-73-165-195.compute-1.amazonaws.com'
-DB_USER = 'sghoswhbrutkep'
-DB_PASS = 'bwc_ko2R3vRhBEWhWZfy2wW6XC'
-DB_NAME = 'd5v2u48i27s9a5'
+DB_HOST = 'imsureyoudliketoknow'
+DB_USER = 'beautyboy'
+DB_PASS = 'yousureaboutthis'
+DB_NAME = 'powerslave'
