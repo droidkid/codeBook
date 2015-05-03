229163450dc5a748985668808007fc4334a5222f
droidkid <karch08@gmail.com>
2015-05-04 00:44:25 +0530

    Binding to  on heroku

--- run.py
+++ run.py
@@ -1,2 +1,5 @@
 from cookBook import app
-app.run(host='0.0.0.0', debug=True);
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT',5000)); #get $PORT from heroku else default 5000
+    app.run(host='0.0.0.0', port = port);
+
