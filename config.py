a4edb03dafa4e5de7948f8bb92e9521c98b0d4b8
droidkid <karch08@gmail.com>
2015-05-04 00:32:21 +0530

    Heroku config updated. Time to test

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DB_HOST = 'imsureyoudliketoknow'
-DB_USER = 'beautyboy'
-DB_PASS = 'yousureaboutthis'
-DB_NAME = 'powerslave'
+DB_HOST = 'ec2-184-73-165-195.compute-1.amazonaws.com'
+DB_USER = 'thbmdqnbbxvrhp'
+DB_PASS = 'yL4l8b9_d1iQPsNPND-AcqmjNl'
+DB_NAME = 'd5v2u48i27s9a5'
